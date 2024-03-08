a0014e0321209f1459dc5b288f5376f3d567443e
Astronants <40693768+Astronants@users.noreply.github.com>
2024-03-08 21:26:20 +0100

    Removed installation checking

---
+++ Source/Utils.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace Decalco
+{
+    internal class Utils
+    {
+        internal static readonly string GameDataDir = UrlDir.CreateApplicationPath("GameData").Replace('\\', '/');
+        internal static readonly string ModDir = Path.Combine(GameDataDir, "Decalc'o'mania");
+    }
+}
